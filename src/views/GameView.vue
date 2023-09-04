3b647e324d6b9c2dbb8a87e02ee0e63573ba2781
Pedro Machado <macpedrot@gmail.com>
2023-09-04 18:54:14 +0100

    start project

---
+++ src/views/GameView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        this is the game page
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+
+    }
+}
+</script>
+
+<style ></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    this is the home view
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+
+  }
+}
+
+</script>
+
+<!-- <script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template> -->
