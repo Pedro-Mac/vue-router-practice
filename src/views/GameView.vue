221ed4dac257ea6d9fda18e600c1714192ba1766
Pedro Machado <macpedrot@gmail.com>
2023-09-14 09:00:03 +0100

    🎨 add unsplash fetching

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,18 +1,27 @@
 <template>
     <div>
         <p v-if="user.username">{{ user.username }}</p>
-        this is the game page
+
     </div>
 </template>
 
 <script>
 import { useUserState } from '../store/user'
+const token = import.meta.env.VITE_UNSPLASH_KEY
 
 export default {
     data() {
         return {
-            user: useUserState()
+            user: useUserState(),
+            photos: [],
         }
+    },
+
+    async created() {
+        const res = await fetch(`https://api.unsplash.com/photos/?client_id=${token}&page=1`)
+        const resData = await res.json()
+        this.photos = resData
+
     }
 }
 </script>
