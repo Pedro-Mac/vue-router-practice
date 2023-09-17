da459caf88a983a9301db3100866db2864dd7f5d
Pedro Machado <macpedrot@gmail.com>
2023-09-17 17:04:07 +0100

    ✨ hide card if not selected nor matched

--- src/components/GameView/GameImage.vue
+++ src/components/GameView/GameImage.vue
@@ -1,15 +1,33 @@
 <template>
-    <div>
-        <p v-if="photo.isMatched">Is matched</p>
-        <p v-else-if="isSelected">Is selected</p>
-        <img :src="photo.src.tiny" :alt="photo.alt_description" @click="selectPhoto(photo)">
+    <div class="container" @click="selectPhoto(photo)">
+        <img :src="photo.src.tiny" :alt="photo.alt_description">
+        <div :class="(isMatched || isSelected) ? '' : 'backside'"></div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['photo', 'selectPhoto', 'isSelected']
+    props: ['photo', 'selectPhoto', 'isSelected'],
+
+    computed: {
+        isMatched() {
+            return this.photo.isMatched
+        },
+    },
 }
 </script>
 
-<style></style>
+<style>
+.container {
+    position: relative;
+}
+
+.backside {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: aliceblue;
+}
+</style>
