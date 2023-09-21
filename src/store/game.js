d2ab2f3cb24ead70c4acab20ec3acb5cc3c4a37e
Pedro Machado <macpedrot@gmail.com>
2023-09-21 19:48:29 +0100

    add game store

---
+++ src/store/game.js
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia'
+
+export const useGameState = defineStore("game", {
+    state: () => ({
+        game: null
+    }),
+
+    actions: {
+        setGame(value){
+            this.game = value
+            localStorage.setItem("game", JSON.stringify(value))
+        },
+
+        getGame(){
+            const game = JSON.parse(localStorage.getItem("game"))
+
+            
+            if(lgameist) {
+                this.game = game;
+                return game
+            }
+        },
+    }
+})
